3aa5e7b594728179c039e1935573b7077998a577
Wadoyoka <atsushi021028@gmail.com>
2024-10-08 11:29:39 +0900

    thank edit

--- app/thank/page.tsx
+++ app/thank/page.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 export default function ThankYou() {
     return (
         <div className="container mx-auto px-4 py-8 min-h-[80vh] flex flex-col items-center justify-center">
-            <h1 className="text-4xl font-bold mb-6">Thank You!</h1>
-            <p className="text-xl mb-8">Your message has been sent successfully. We`&apos;`ll get back to you soon.</p>
+            <h1 className="text-4xl font-bold mb-6">ありがとうございます!</h1>
+            <p className="text-xl mb-8">メッセージは送信されました！</p>
             <Link href="/" className="text-blue-600 hover:underline">
                 Return to Home
             </Link>
