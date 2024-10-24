c533373dd37c86c538fd5679936e9ad33d1e78eb
Wadoyoka <atsushi021028@gmail.com>
2024-10-24 12:47:25 +0900

    sidebar color

--- components/layouts/header/HamburgerMenu.tsx
+++ components/layouts/header/HamburgerMenu.tsx
@@ -2,24 +2,50 @@
 
 import { Button } from "@/components/ui/button"
 import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "@/components/ui/sheet"
-import { Menu } from 'lucide-react'
+import { House, Mail, Menu, Newspaper, User } from 'lucide-react'
 import Link from 'next/link'
+import { usePathname } from "next/navigation"
 import { useState } from 'react'
 
 interface MenuItem {
     href: string
     label: string
+    icon: string
 }
 
 const menuItems: MenuItem[] = [
-    { href: '/', label: 'Top' },
-    { href: '/about', label: 'About' },
-    { href: '/blog', label: 'Blog' },
-    { href: '/contact', label: 'Contact' },
+    { href: '/', label: 'Top', icon: 'House' },
+    { href: '/about', label: 'About', icon: 'User' },
+    { href: '/blog', label: 'Blog', icon: 'News' },
+    { href: '/contact', label: 'Contact', icon: 'Mail' },
 ]
 
+function selectIcon(icon: string) {
+    switch (icon) {
+        case 'House':
+            return (
+                <House />
+            );
+        case 'User':
+            return (
+                <User />
+            );
+        case 'News':
+            return (
+                <Newspaper />
+            );
+        case 'Mail':
+            return (
+                <Mail />
+            );
+        default:
+            return null;
+    }
+}
+
 export default function HamburgerMenu() {
-    const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(false);
+    const pathname = usePathname();
 
     return (
         <Sheet open={isOpen} onOpenChange={setIsOpen}>
@@ -29,7 +55,7 @@ export default function HamburgerMenu() {
                 </Button>
             </SheetTrigger>
             <SheetContent side="right" className="w-[300px] sm:w-[400px]">
-                <SheetTitle  className="sr-only">Menu</SheetTitle>
+                <SheetTitle className="sr-only">Menu</SheetTitle>
                 <SheetDescription className="sr-only">
                     Navigate through our site using the links below.
                 </SheetDescription>
@@ -38,9 +64,15 @@ export default function HamburgerMenu() {
                         <Link
                             key={item.href}
                             href={item.href}
-                            className="text-lg font-medium hover:text-primary transition-colors"
+                            className={`flex text-lg font-medium transition-colors ${(pathname === item.href)
+                                ? 'text-blue-600 hover:text-blue-700'
+                                : 'text-foreground hover:text-primary'
+                                }`}
                             onClick={() => setIsOpen(false)}
                         >
+                            <div className="mr-2">
+                                {selectIcon(item.icon)}
+                            </div>
                             {item.label}
                         </Link>
                     ))}
